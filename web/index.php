5694640db26e13a283adc2bcd9a9bc0f3a460471
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-03-08 00:33:19 +0100

    Layout angefangen

--- web/index.php
+++ web/index.php
@@ -1,5 +1,170 @@
-<?php
+<html lang="en">
+    <head>
+        <title>Web World</title> 
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 
-echo "test";
-echo "hi ich bims";
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/navi.css">
+    </head>
 
+    <script>
+        $(document).ready(function () {
+            isClosed = false;
+            var $open = $(".hh-toggle-btn");
+
+            $open.click(function () {
+                $(".hh-logo-container").toggleClass("hh-nav-min");
+                $(".hh-sidebar").toggleClass("hh-nav-min");
+                $(".hh-body-wrapper").toggleClass("hh-nav-min");
+                return false;
+
+            });
+        });
+    </script>  
+    <body>
+
+        <!-- Start Header -->
+        <div class="header">
+            <div class="top-header">
+                <div class="container">
+                    <h2>Web<span>World</span></h2>              
+                </div>
+            </div>     
+            <div class="slider">
+                <div class="container">
+                    <div class="intro">
+                        <h2>Welcome To Our Web Site</h2>
+                        <p>Lorem ipsum dolor sit amet, ea doming until epicuri iudicabit nam, te usu virtute placerat. Purto brute disputando cu est.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="top-header">
+                <div class="container">
+                    <center><a href="#" class="hh-toggle-btn">Seite1</a>   
+                    <a href="#" class="hh-toggle-btn">Seite2</a>  
+                    <a href="#" class="hh-toggle-btn">Seite3</a>                       
+                </center></div>
+            </div>  
+        </div>
+        <!-- End Header -->
+
+        <div class="container">
+            <navi>
+                <div class="flat-theme">  
+                    <nav role="navigation">
+                        <section>
+                            <header>
+                                <div class="hh-logo-container">
+                                    <br>OptiLearn
+                                    <a href="#" class="hh-toggle-btn"><i style='margin-top: 15px;' class="fa fa-bars"></i></a>
+                                </div>
+
+                            </header>
+                        </section>
+                        <br><br><br>
+                        <aside>
+                            <nav class="hh-sidebar">
+                                <ul>
+                                    <li>
+                                        <a href="#" class="inbox">
+                                            <i class="fa fa-dashboard"><span class="icon-bg hh-bg-success"></span></i>
+                                            <span class="hh-sidebar-item">Dashboard</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-bar-chart-o">
+                                                <span class="icon-bg hh-bg-danger"></span></i>
+                                            <span class="hh-sidebar-item">Ticket status</span>
+                                        </a>
+                                    </li>
+                                    <li><a href="#" class="snooz"><i class="fa fa-line-chart"><span class="icon-bg hh-bg-primary"></span></i><span class="hh-sidebar-item">Call trends</span></a></li>
+                                    <li><a href="#" class="done"><i class="fa fa-area-chart"><span class="icon-bg hh-bg-warning"></span></i><span class="hh-sidebar-item">Heat maps</span></a></li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-wrench"><span class="icon-bg hh-bg-violet"></span></i><span class="hh-sidebar-item">Settings</span></a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </aside>   
+                    </nav>
+                </div>
+            </navi>
+
+            <section>
+               <h2>Welcome To Our Web Site</h2>
+               
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when                 1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.n pLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.rinter took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.n pLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.rinter took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.n pLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                    1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.rinter took a galley of type and scrambled it to make atype specimen book.<br>
+                    It has survived not only five centuries.
+             
+            </section>
+        </div>
+        
+        <!-- Start Contact Me -->
+        <div class="contact-me">
+            <div class="container">
+                <div class="details">
+                    <div> 
+                        <h3>Überschrift</h3> 
+                        <p>Text</p>
+                    </div>          
+                    <div>
+                        <h3>Tags</h3>
+                        <p>Text</p>
+                    </div>      
+                    <div>
+                        <h3>A little about us</h3>
+                        <p>Text</p>
+                    </div>      
+                    <div>
+                        <h3>Get in touch</h3>
+                        <p>Text</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Contact Me -->
+        <!-- Start Footer -->
+        <div class="footer">
+            <div class="container">       
+            </div>
+        </div>
+        <!-- End Footer -->
+    </body>
