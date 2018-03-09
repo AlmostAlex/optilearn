f0af6018899f14d513dae9e09b29d75b12dda72f
AlmostAlex <34305685+AlmostAlex@users.noreply.github.com>
2018-03-09 13:13:47 +0100

    Login Controller und Model

--- web/controller/index_controller.php
+++ web/controller/index_controller.php
@@ -7,38 +7,37 @@ class index_controller
 
     //Erstellen einer Variable vom Typ benutzer_model, 
     //damit alle Funktionen Zugriff auf diese über $this->modul haben
-    public $modul;
+    public $benutzer;
 
     public function __construct() 
     {
-        $this->modul = new modul_model();
+        $this->benutzer = new benutzer_model();
     }
 
     //Eintragung des Bewerbers in dem Modul (windhund)
-    public function login() 
+    public function login()
     {
-        $password = $_POST["passwort"];
-        $benutzer = $_POST["benutzername"];
+        $passwort = $_POST["passwort"];
+        $benutzername = $_POST["benutzername"];
         if (!empty($password) && !empty($benutzer))
-        {  
-            $pass_corr = $this->model->LoginKontrolle($benutzer,$password);
+        {
+            $pass_corr = $this->benutzer->LoginKontrolle($benutzername, $passwort);
             
-            if ($pass_corr == TRUE) 
+            if ($pass_corr == TRUE)
             {
-
-                $_SESSION['login'] = $this->model->getId($benutzer); // Loggt einen ein!           
+                $_SESSION['login'] = $this->benutzer->getBenutzerId($benutzername); // Loggt einen ein!
                 echo"<div class='alertlogin'><div class='alert alert-success' role='alert'><b>Anmeldung war erfolreich!</b><br>Die Weiterleitung erfolgt in wenigen Sekunden. <br> <img src='img/ajax-loader.gif'></div></div>";
                 echo "<meta http-equiv='refresh' content='1.5; URL=/verwaltung.php'>"; // Weiterleitung zur Verwaltung 
-            }           
+            }
             else 
             {
-                echo "<div class='alertlogin'><div class='alert alert-danger role='alert'><b>Achtung!</b><br>Das Passwort und der Benutzername stimmen nicht Ã¼berein.</div></div>";   
-            }  
+                echo "<div class='alertlogin'><div class='alert alert-danger role='alert'><b>Achtung!</b><br>Das Passwort und der Benutzername stimmen nicht überein.</div></div>";   
+            }
         }
         else 
-        { 
+        {
             echo "<div class='alertlogin'><div class='alert alert-danger' role='alert'><b>Achtung!</b><br>Bitte fÃ¼lle alle Eingabefelder aus!</div></div>";      
-        }                
+        }
     }
 
     public function registrieren() 
---
+++ web/model/benutzer_model.php
@@ -0,0 +1,43 @@
+<?php
+
+class benutzer_model 
+{
+    //Erstellen einer Variable $dbh und speichern des Datenabnkzugriffs auf dieser
+    //damit alle Funktionen Zugriff auf diese über $this->dbh haben
+    public $dbh;
+
+    public function __construct() 
+    {
+        require("db.php");
+        $this->dbh = $dbh;
+    }
+
+    public function getBenutzerId($benutzername) 
+    {
+        $statement = $this->dbh->prepare(
+            "SELECT benutzer_id 
+            FROM benutzer 
+            WHERE benutzername = ?");
+
+        $statement->bind_param('s', $benutzername);
+        $statement->execute();
+        $statement->bind_result($benutzer_id);
+        while ($statement->fetch()) 
+        {
+            return $benutzer_id;
+        }
+    }
+
+    public function LoginKontrolle($benutzername, $passwort) 
+    {
+        $statement = $this->dbh->prepare("SELECT passwort FROM benutzer WHERE benutzername = ?");
+        $statement->bind_param('s', $benutzername);
+        $statement->execute();
+        $statement->bind_result($pw);
+        while ($statement->fetch()) 
+        {
+            return ($pass_corr = password_verify($passwort, $pw));
+        }
+        
+    }
+}
