17960371cdea2830ecc189aec0de94f742e85053
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-03-09 17:33:16 +0100

    Layout fertig

--- web/index.php
+++ web/index.php
@@ -1,124 +1,127 @@
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>OptiLearn -  optimiertes Lernen!</title> 
-        <meta charset="utf-8">
-        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>OptiLearn - effizienteres Arbeiten</title>
         <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        
-        
-        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
 
-        <link rel="stylesheet" href="css/main.css">
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Varela+Round" rel="stylesheet" />
+        <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+        
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>      
     </head>
-
     <body>
-        <!-- Start Header -->  
-        <div class="header">
-            <div class="top-header">
-                <div class='right'>
-                    <div class='linkages'><a style='border-right: 1px solid #838383' href="">Home</a></div>
-                    <div class='linkages'><a style='border-right: 1px solid #838383' href="">About us</a></div>
-                    <div class='linkages'><a href="">Contact</a></div>
-                </div>
-            </div>     
-
-
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <div class="slider">
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner">
-                        <div class="item active"><first><h3>OptiLearn</h3><br><h4>Optimales Lernen durch optimale Bedingungen!</h4> </first></div>
-                        <div class="item"> nummer2</div>
-                        <div class="item"> nummer3</div>
+        <div id="wrapper">
+            <div id="header-wrapper">
+                <div id="header" class="container">
+                    <div id="logo">
+                        <h1><a href="#">OptiLearn</a></h1>
+                        <p>Optimales Lernen durch optimale Bedingungen!</p>
                     </div>
-
-                    <!-- Left and right controls -->
-                    <a class="left carousel-control" style='margin-left: 19px;' href="#myCarousel" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" style='margin-right: 19px;' href="#myCarousel" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>     
+                    <div id="social">
+                        <ul class="contact">
+                            <li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+                            <li><a href="#" class="icon icon-facebook"><span></span></a></li>
+                            <li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>
+                            <li><a href="#" class="icon icon-tumblr"><span>Google+</span></a></li>
+                            <li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="menu" class="container">
+                    <ul>
+                        <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+                        <li><a href="#" accesskey="1" title="">Services</a></li>
+                        <li><a href="#" accesskey="2" title="">Our Clients</a></li>
+                        <li><a href="#" accesskey="3" title="">About Us</a></li>
+                        <li><a href="#" accesskey="4" title="">Careers</a></li>
+                        <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+                    </ul>
+                </div>
             </div>
+            <div id="page" class="container">
 
-            <div class="bottom-header"> <br>
-            </div>  
-        </div>
-        <!-- End Header -->
+                <div>              
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
 
-        <div class="container">   
-            <navi>
-                <div class="theme">  
-                    <section>
-                        <div class="hh-logo-container">
-                            Navigation
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="images/pic03.jpg" alt="Los Angeles" style="width:100%;">
+                            </div>
+
+                            <div class="item">
+                                <img src="images/pic03.jpg" alt="Chicago" style="width:100%;">
+                            </div>
+
+                            <div class="item">
+                                <img src="images/pic03.jpg" alt="New york" style="width:100%;">
+                            </div>
                         </div>
-                    </section>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" style='height: 96%;' href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" style='height: 96%;' href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>           
+                <div class="column1">
+                    <div class="title">
+                        <h2>Maecenas luctus</h2>
+                        <span class="byline">Pellentesque lectus gravida blandit</span>
+                    </div>
+                    <p> content bereich ntum nibh augue praesent a lacus at urna congue rutntum nibh augue praesentum nibh
+                        augue praesent a lacus at urna congue rutnt a lacus at urnantum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum n
+                        ibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a
+                        lacus at urna congue rut congue rutntum nibh augue praesent a lacu
+                        s at urna congue rutntum nibh augue praesent a lacus at urna congue rut </p>
+                    <a href="#" class="button">Etiam posuere</a>
                 </div>
-                <div class="flat-theme">  
-                    <aside>
-                        <nav class="hh-sidebar">
+
+                <div class="column3">
+                    <navi>
+                        <div class="title">
+                            <h2>Navigationsbreich</h2>
+                        </div>
+                        <p>
                             <ul>
-                                <li>
-                                    <a href="#" class="inbox"><i class="fa fa-dashboard"></i><span class="hh-sidebar-item">Dashboard</span></a></li>
-                                <li>
-                                    <a href="#"><i class="fa fa-bar-chart-o"> </i><span class="hh-sidebar-item">Ticket status</span></a>
-                                </li>
-                                <li><a href="#" class="snooz"><i class="fa fa-line-chart"></i><span class="hh-sidebar-item">Call trends</span></a></li>
-                                <li><a href="#" class="done"><i class="fa fa-area-chart"></i><span class="hh-sidebar-item">Heat maps</span></a></li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa fa-wrench"></i><span class="hh-sidebar-item">Settings</span></a>
-                                </li>
+                                <li><a class='linkages' href="">Home</a></li>
+                                <li><a class='linkages' href="">Home</a></li>
+                                <li><a class='linkages' href="">Home</a></li> 
                             </ul>
-                        </nav>
-                    </aside>
-                </div>
-            </navi>
-            <section>
-                <h2><i class="fa fa-square"></i> Welcome To Our Web Site</h2>  
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>
-                <h2><i class="fa fa-square"></i> Welcome To Our Web Site</h2>  
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen boo Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen boo Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknowLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen book.<br>         
-            </section> 
+                        </p>
+                    </navi>
+                </div>      
+            </div>
         </div>
-        <!-- Start Contact Me -->
-        <div class="footer">
+        </div>
+
+        <div id="footer">
             <div class="details">
                 <div> 
                     <h3>Kontakt</h3> 
                     <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen </p>
+                            1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen </p>
                 </div>          
                 <div>
                     <h3>Information</h3>
                     <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype nd scrambled it to make atype specimen </p>
+                            1500s, when an unknown printer took a galley of type and scrambled it to make atype nd scrambled it to make atype specimen </p>
                 </div>      
                 <div>
                     <h3>Wissenschaftliches</h3>
@@ -131,6 +134,6 @@
                 </div>  
             </div>
         </div>
-        <div class='footer-end'></div>
-        <!-- End Contact Me -->
+
     </body>
+</html>
