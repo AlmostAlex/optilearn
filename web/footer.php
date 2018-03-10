b75b5ba9f29d53b6c7cf4a0201b6b96c31c6ff21
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-03-10 13:20:32 +0100

    footer

--- web/footer.php
+++ web/footer.php
@@ -1,26 +1,27 @@
-<div id="footer">
-    <div class="details">
-        <div> 
-            <h3>Kontakt</h3> 
-            <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen </p>
-        </div>          
-        <div>
-            <h3>Information</h3>
-            <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
-                1500s, when an unknown printer took a galley of type and scrambled it to make atype nd scrambled it to make atype specimen </p>
-        </div>      
-        <div>
-            <h3>Wissenschaftliches</h3>
-            <p>nd scrambled it to make atype nd scrambled it to make atype nd scrambled it to make atype </p>
-        </div>  
-        <div>
-            <h3>Example</h3>
-            <p>nd scrambled it to make atype nd scrambled it to make atype nd scrambled it to me atype nd scrambled it to make 
-                atype nd scrambled it to me atype nd scrambled it to make atype nd scrambled it to make atype </p>
-        </div>  
-    </div>
-</div>
 
-</body>
+        <div id="footer">
+            <div class="details">
+                <div> 
+                    <h3>Kontakt</h3> 
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                            1500s, when an unknown printer took a galley of type and scrambled it to make atype specimen </p>
+                </div>          
+                <div>
+                    <h3>Information</h3>
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever since the <br>
+                            1500s, when an unknown printer took a galley of type and scrambled it to make atype nd scrambled it to make atype specimen </p>
+                </div>      
+                <div>
+                    <h3>Wissenschaftliches</h3>
+                    <p>nd scrambled it to make atype nd scrambled it to make atype nd scrambled it to make atype </p>
+                </div>  
+                <div>
+                    <h3>Example</h3>
+                    <p>nd scrambled it to make atype nd scrambled it to make atype nd scrambled it to me atype nd scrambled it to make 
+                        atype nd scrambled it to me atype nd scrambled it to make atype nd scrambled it to make atype </p>
+                </div>  
+            </div>
+        </div>
+
+    </body>
 </html>
