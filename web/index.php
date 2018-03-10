97eb2de2998249b89cdb42e936ca51d0c2eaeb64
AlmostAlex <34305685+AlmostAlex@users.noreply.github.com>
2018-03-10 16:46:03 +0100

    index

--- web/index.php
+++ web/index.php
@@ -94,11 +94,11 @@ require("controller/index_controller.php");
     </div>
 </div>
 <! ----------------------------------------------------------->
-<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<script src="vendor/bootstrap/js/popper.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="vendor/select2/select2.min.js"></script>
-<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script src="java/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="java/vendor/bootstrap/js/popper.js"></script>
+<script src="java/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="java/vendor/select2/select2.min.js"></script>
+<script src="java/vendor/tilt/tilt.jquery.min.js"></script>
 <script>
 		$('.js-tilt').tilt({
 			scale: 1.1
