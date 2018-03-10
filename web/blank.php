c9d0f3b362fe06173b4ac81dd20f0706615149ce
AlmostAlex <34305685+AlmostAlex@users.noreply.github.com>
2018-03-10 19:26:08 +0100

    animation vom login

--- web/blank.php
+++ web/blank.php
@@ -1,101 +1,165 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>Contact V1</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-</head>
-<body>
+<?php
+include('header.php');
+require("controller/index_controller.php");
+require("db.php");
+?>
 
-	<div class="contact1">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<div class="title">
+    <span class="byline">Fragen und Antworten</span>
+</div> 
+
+<answer>
+    <ul>
+        <li>
+            <div class="panel-heading"><a style='border-top: 1px dotted #000000;' class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse1">Results</a></div>
+        </li>      
+        <div id="collapse1" class="panel-collapse collapse">
+            <div class="panel-body">
+                ongue rutnt a lacus atongue rutnt a lacus at urnantum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum n
+                ibh augue praesent a lacus at urna congue rutntum nibh augue praesenongue rutnt a lacus at urnantum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum n
+                ibh augue praesent a lacus at urna congue rutntum nibh augue praesen urnantum nibh augue praesent
+            </div>
+        </div>  
+
+        <li>
+            <div class="panel-heading"><a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse2">Results</a></div>
+        </li>      
+        <div id="collapse2" class="panel-collapse collapse">
+            <div class="panel-body">
+                ongue rutnt a lacus atongue rutnt a lacus at urnantum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum n
+                ibh augue praesent a lacus at urna congue rutntum nibh augue praesenongue rutnt a lacus at urnantum nibh augue praesent a lacus at urna congue rutntum nibh augue praesent a lacus at urna congue rutntum n
+                ibh augue praesent a lacus at urna congue rutntum nibh augue praesen urnantum nibh augue praesent
+            </div>
+        </div> 
+    </ul>
+</answer>
+
+<script>
+function showBezirk(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        $("#waehle_stadt").slideDown();
+        $("#bezirk").slideUp();
+        return;
+    } else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+                $("#waehle_stadt").slideUp();
+                $("#bezirk").slideDown();
+            }
+        };
+        xmlhttp.open("GET","getBezirk.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+</script>
+<br><br>
+<center><div class="title">
+        <h2>Optilearn - Login-System</h2>
+        <span class="byline">Als registrierter User oder Free-User!</span>
+    </div> 
+</center>
+
+<?php
+ $control = new index_controller();
+ $control->free_user_formular();
+  ?>
+
+<br><br><br>
+<div class="contact1">
 		<div class="container-contact1">
 			<div class="contact1-pic js-tilt" data-tilt>
 				<img src="images/img-01.png" alt="IMG">
+			<!--    EinfÃ¼gen der Form zum einloggen    -->
 			</div>
-
-			<form class="contact1-form validate-form">
-				<span class="contact1-form-title">
-					Get in touch
-				</span>
-
-				<div class="wrap-input1 validate-input" data-validate = "Name is required">
-					<input class="input1" type="text" name="name" placeholder="Name">
-					<span class="shadow-input1"></span>
+<div class='logform'>
+<form method='post'>
+	<table style='width: 90%; margin: 5%;'>
+		<tr>
+			<td colspan='3'><h4 class='card-title'><i class="fa fa-arrow-circle-o-down" aria-hidden="true"></i> Login-Bereich</h4></td>
+		</tr>
+		<tr>      
+			<td>                
+				<div class="input-group">
+					<span style='background-color: white; padding-left: 15px; padding-right: 14px;' class="input-group-addon"> <i class="fa fa-user" aria-hidden="true"></i></span>
+					<input type='text' class='form-control' placeholder="Benutzername" required name='benutzername' required>
 				</div>
-
-				<div class="wrap-input1 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					<input class="input1" type="text" name="email" placeholder="Email">
-					<span class="shadow-input1"></span>
+			</td>
+		</tr>
+		<tr>
+			<td>                
+				<div class="input-group">
+					<span style='background-color: white;' class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+					<input type='password' class='form-control' placeholder="Passwort" required name='passwort' required>
 				</div>
-
-				<div class="wrap-input1 validate-input" data-validate = "Subject is required">
-					<input class="input1" type="text" name="subject" placeholder="Subject">
-					<span class="shadow-input1"></span>
-				</div>
-
-				<div class="wrap-input1 validate-input" data-validate = "Message is required">
-					<textarea class="input1" name="message" placeholder="Message"></textarea>
-					<span class="shadow-input1"></span>
-				</div>
-
-				<div class="container-contact1-form-btn">
-					<button class="contact1-form-btn">
-						<span>
-							Send Email
-							<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-						</span>
-					</button>
-				</div>
-			</form>
-		</div>
+			</td>
+		</tr>
+		<tr>      
+			<td>
+				<br><input style='padding-left: 7%; padding-right: 7%;  float:right; ' type='submit' class='buttons' name='einloggen' value='Login'></td>
+		</tr>
+	</table>
+</form>
+</div>
 	</div>
 
 
 
 
-<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-23581568-13');
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/select2/select2.min.js"></script>
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script >
+	$('.js-tilt').tilt({
+		scale: 0.9
+	})
 </script>
 
-<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+if (isset($_POST["einloggen"])) 
+{
+    $control = new index_controller();
+    $control->login();
+}
+
+include('navi.php');
+include('footer.php');
+?>
