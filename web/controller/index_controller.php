659553ad26bb10f158e23e5b997f70bc946bd62d
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-03-10 15:32:39 +0100

    ajax

--- web/controller/index_controller.php
+++ web/controller/index_controller.php
@@ -33,41 +33,65 @@ class index_controller {
     }
 
     public function free_user_formular() {
-        $statement = $this->stadt->getAllBezeichnung();
-        $statement->bind_result($stadtbezeichnung, $stadt_id);
-        $statement->store_result();
         ?>
-        <form>
-            <label>Stadt</label>
-            <select name="users" onchange="showUser(this.value)">
-        <?php
-            echo "<option value=''>Wähle eine Stadt aus!</option>";
-        while ($statement->fetch()) {
-            echo "<option value=''>{$stadtbezeichnung}</option>";
-        }
-        ?>
-            </select>
-        </form>
-<?php
-    }
-
-    public function registrieren() {
-        $password = $_POST["passwort"];
-        $benutzer = $_POST["benutzername"];
-        if (!empty($password) && !empty($benutzer)) {
-            $pass_corr = $this->model->LoginKontrolle($benutzer, $password);
-
-            if ($pass_corr == TRUE) {
-
-                $_SESSION['login'] = $this->model->getId($benutzer); // Loggt einen ein!           
-                echo"<div class='alertlogin'><div class='alert alert-success' role='alert'><b>Anmeldung war erfolreich!</b><br>Die Weiterleitung erfolgt in wenigen Sekunden. <br> <img src='img/ajax-loader.gif'></div></div>";
-                echo "<meta http-equiv='refresh' content='1.5; URL=/verwaltung.php'>"; // Weiterleitung zur Verwaltung 
-            } else {
-                echo "<div class='alertlogin'><div class='alert alert-danger role='alert'><b>Achtung!</b><br>Das Passwort und der Benutzername stimmen nicht Ã¼berein.</div></div>";
+        <script>
+            <script>
+                function showCD(str) {
+                        if (str == "") {
+                document.getElementById("txtHint").innerHTML= "";         return;
+            } 
+            if (window.XMLHttpRequest) {
+                        // code for IE7+, Firefox, Chrome, Opera, Safari
+                        xmlhttp = new XMLHttpRequest();
+                } else {  // code for IE6, IE5
+                        xmlhtt p  = new ActiveXObject("Microsoft.XMLHTTP");
             }
-        } else {
-            echo "<div class='alertlogin'><div class='alert alert-danger' role='alert'><b>Achtung!</b><br>Bitte fÃ¼lle alle Eingabefelder aus!</div></div>";
-        }
-    }
+            xmlhttp.onreadystatechange=function() {
+                if (this.readyState == 4 && this.status == 200) {
+                document.ge t Elem entById("txtHint").innerHTML = this.responseText;
+                }
+                }
+                xmlhttp.open("GET","/ajax/getbezirk.php?q="+str,true);
+                    xmlhttp.send();
+                    }
+        </script>
+            <?php
+            $statement = $this->stadt->getAllBezeichnung();
+            $statement->bind_result($stadtbezeichnung, $stadt_id);
+            $statement->store_result();
+            ?>
+                    <form>
+                        <label>Stadt</label>
+                        <select name="users" onchange="showStadtbezirk(this.value)">
+                            <?php
+                            echo "<option value=''>Wähle eine Stadt aus!</option>";
+                            while ($statement->fetch()) {
+                                echo "<option value=''>{$stadtbezeichnung}</option>";
+                            }
+                            ?>
+                        </select>
+                    </form>
+                    <?php
+                }
 
-}
+                public function registrieren() {
+                    $password = $_POST["passwort"];
+                    $benutzer = $_POST["benutzername"];
+                    if (!empty($password) && !empty($benutzer)) {
+                        $pass_corr = $this->model->LoginKontrolle($benutzer, $password);
+
+                        if ($pass_corr == TRUE) {
+
+                            $_SESSION['login'] = $this->model->getId($benutzer); // Loggt einen ein!           
+                            echo"<div class='alertlogin'><div class='alert alert-success' role='alert'><b>Anmeldung war erfolreich!</b><br>Die Weiterleitung erfolgt in wenigen Sekunden. <br> <img src='img/ajax-loader.gif'></div></div>";
+                            echo "<meta http-equiv='refresh' content='1.5; URL=/verwaltung.php'>"; // Weiterleitung zur Verwaltung 
+                        } else {
+                            echo "<div class='alertlogin'><div class='alert alert-danger role='alert'><b>Achtung!</b><br>Das Passwort und der Benutzername stimmen nicht Ã¼berein.</div></div>";
+                        }
+                    } else {
+                        echo "<div class='alertlogin'><div class='alert alert-danger' role='alert'><b>Achtung!</b><br>Bitte fÃ¼lle alle Eingabefelder aus!</div></div>";
+                    }
+                }
+
+            }
+            
