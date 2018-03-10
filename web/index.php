235e026441a026f2eb5cdd51ed79c488c642f76e
chimidoro <33393205+chimidoro@users.noreply.github.com>
2018-03-10 15:00:37 +0100

    con

--- web/index.php
+++ web/index.php
@@ -1,59 +1,9 @@
 <?php
 include('header.php');
+require("db.php");
+require("controller/index_controller.php");
 ?>
 
-<style>
-#process-box-1{
-  width: 630px;
-  height: 270px;
-  background: rgb(245, 245, 245);
-  margin-left: 220px;
-  border-radius: 5px;
-}
-.process-box-number {
-  width: 100px;
-  border-radius: 3px;
-  height: 100px;
-  margin-left: -10px;
-  background: rgb(250, 194, 0);
-  color: white;
-  z-index: 99999;
-}
-.process-box-number p {
-  font-size: 45px;
-  font-family: 'Montserrat', sans-serif;
-  padding-top: 21px;
-  padding-left: 20px;
-}
-.triangle-process {
-  margin-left: -10px;
-  width: 0;
-  height: 0;
-  border-top: 10px solid rgb(173, 136, 0);
-  border-left: 10px solid transparent;
-  margin-top: -1px;
-  z-index: -99999;
-}
-.process-text-head {
-  margin-top: -115px;
-  padding-top: 20px;
-  padding-left: 110px;
-  color: rgb(51, 51, 51);
-  font-size: 30px;
-  font-weight: 600;
-  font-family: 'Montserrat', sans-serif;
-}
-.process-text-desc {
-  padding-top: 15px;
-  padding-left: 110px;
-  padding-right: 40px;
-  font-size: 15px;
-  font-weight: 400;
-  font-family: 'Montserrat', sans-serif;
-  letter-spacing: 0px;
-  line-height: 23px;
-}
-</style>
 <div class="title">
     <span class="byline">Fragen und Antworten</span>
 </div> 
@@ -90,17 +40,20 @@ include('header.php');
     <span class="byline">Als registrierter User oder Free-User!</span>
 </div> 
 </center>
-  <div id="process-box-1">
-    <div class="process-box-number">
-    <p>01.</p>
-    </div>
-    <div class="triangle-process"></div>
-    <div class="process-text-head">Free-User</div>
-    <div class="process-text-desc">We're all about getting it done on time and to client satisfaction. From start to finish we have a positive and enthusiastic attitude. 
-        We have the experience and dedication to finish all our projects on time giving all our clients peace of mind.<br><a href="#" class="button">Etiam posuere</a></div>
-  </div>
+
+<form action="">
+    <label>Stadt:</label>
+<select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+        <option selected>Wähle eine Stadt aus.</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
 
 
+Ort:
+
+</form>
 
 </div>
 
